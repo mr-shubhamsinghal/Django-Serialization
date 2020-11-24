f8970b47c98dc9721d5ba16677312af71d460b41
Shubham Singhal <singhal121997@gmail.com>
2020-11-24 09:54:53 +0530

    fix code

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,2 @@
 
 from django.contrib import admin
-
-from blog.models import BlogModel
-
-
-admin.site.register(BlogModel)
--- blog/serializers.py
+++ blog/serializers.py
@@ -1,11 +1,2 @@
 
 from rest_framework import serializers
-
-from blog.models import BlogModel
-
-
-class BlogSerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = BlogModel
-		fields = '__all__'
